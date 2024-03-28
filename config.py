339560387d87a12998331be684aef83121987984
karinleanne <karin.leanne@hotmail.com>
2024-03-28 10:59:43 +0100

    test config file

--- config.py
+++ config.py
@@ -2,8 +2,12 @@
 # A configuration file with hyperparameters.
 ###
 
-n_steps = 560             # Must be a multiple of 8
-n_agents = 500
+labelsize = 20
+titlesize = 22
+ticksize = 16
+
+n_steps = 16             # Must be a multiple of 8
+n_agents = 8
 n_rounds = 3
 
 rewiring_p = 0.6           # Has to be between 0 and 1
