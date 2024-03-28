f688dd69764b88e0a70c77e9f98f2f9240dc3de4
karinleanne <karin.leanne@hotmail.com>
2024-03-28 11:26:42 +0100

    to run on machine

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ labelsize = 20
 titlesize = 22
 ticksize = 16
 
-n_steps = 16             # Must be a multiple of 8
-n_agents = 8
+n_steps = 480             # Must be a multiple of 8
+n_agents = 300
 n_rounds = 3
 
 rewiring_p = 0.6           # Has to be between 0 and 1
